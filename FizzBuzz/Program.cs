fd48b7dd69a46b32df5efe2a01ff9b7552beeb01
Bailey Hart <baileyhart100@gmail.com>
2023-09-06 11:27:28 -0400

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace FizzBuzz 
+{ 
+    public class Program
+    {
+        public string FizzBuzz(int number)
+        {
+            var answer = "";
+
+            if (number % 3 == 0)
+            {
+                answer = "fizz";
+            }
+
+            if (number % 5 == 0)
+            {
+                answer += "buzz";
+            }
+
+            return answer;
+        }
+    }
+}
+
+
